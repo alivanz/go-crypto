39d504dc4f551d57677b5cbd4b06429b13f1b7ef
Alivan Akbar <alivan1627@gmail.com>
2018-09-20 13:24:52 +0700

    address thing

--- bitcoin/address.go
+++ bitcoin/address.go
@@ -2,43 +2,67 @@ package bitcoin
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"fmt"
 
 	"github.com/alivanz/go-crypto/bitcoin/base58"
+	"golang.org/x/crypto/ripemd160"
 )
 
 var (
 	InvalidChecksum = fmt.Errorf("Invalid Checksum")
 )
 
+const (
+	BitcoinPubKeyHash byte = 0x00
+	BitcoinScriptHash byte = 0x05
+)
+
 type Address [25]byte
 
-func (addr *Address) ParseBase58(address string) error {
+func AddressParseBase58(address string) (*Address, error) {
+	var addr Address
 	data, err := base58.Decode(address)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	if len(data) != 25 {
-		return fmt.Errorf("invalid data length")
+		return nil, fmt.Errorf("invalid data length")
 	}
 	checksum1 := data[21:25]
 	checksum2 := BitcoinHasher.Hash(data[:21])[:4]
 	if !bytes.Equal(checksum1, checksum2) {
-		return InvalidChecksum
+		return nil, InvalidChecksum
 	}
 	copy(addr[:], data)
-	return nil
+	return &addr, nil
 }
-func (addr *Address) ParsePubKeyHash(pubkeyhash []byte) error {
-	if len(pubkeyhash) != 20 {
-		return fmt.Errorf("invalid pubkeyhash length")
+func PubKeyToAddress(addrtype byte, pubkey []byte) (*Address, error) {
+	if len(pubkey) != 65 && len(pubkey) != 33 {
+		return nil, fmt.Errorf("not valid pubkey length (65 or 33 bytes)")
 	}
+	// sha256
+	hash1 := sha256.Sum256(pubkey)
+	// ripemd160
+	ripmd := ripemd160.New()
+	ripmd.Write(hash1[:])
+	hash2 := ripmd.Sum(nil)
+	// copy
+	return PubKeyHashToAddress(addrtype, hash2[:])
+}
+
+func PubKeyHashToAddress(addrtype byte, pubkeyhash []byte) (*Address, error) {
+	var addr Address
+	if len(pubkeyhash) != 20 {
+		return nil, fmt.Errorf("invalid pubkeyhash length")
+	}
+	addr[0] = addrtype
 	copy(addr[1:21], pubkeyhash)
 	checksum := BitcoinHasher.Hash(addr[:21])[:4]
 	copy(addr[21:25], checksum)
-	return nil
+	return &addr, nil
 }
-func (addr *Address) Network() byte {
+func (addr *Address) Version() byte {
 	return addr[0]
 }
 func (addr *Address) String() string {
---
+++ bitcoin/address_test.go
@@ -0,0 +1,70 @@
+package bitcoin
+
+import (
+	"encoding/hex"
+	"testing"
+)
+
+func TestAddress(t *testing.T) {
+	// Uncompressed pubkey
+	if pubkeyx, err := hex.DecodeString("0450863AD64A87AE8A2FE83C1AF1A8403CB53F53E486D8511DAD8A04887E5B23522CD470243453A299FA9E77237716103ABC11A1DF38855ED6F2EE187E9C582BA6"); err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	} else if addr, err := PubKeyToAddress(BitcoinPubKeyHash, pubkeyx); err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	} else if addr.String() != "16UwLL9Risc3QfPqBUvKofHmBQ7wMtjvM" {
+		t.Logf("%v != 16UwLL9Risc3QfPqBUvKofHmBQ7wMtjvM", addr.String())
+		t.Logf(hex.EncodeToString(addr.PubKeyHash()))
+		t.Fail()
+		return
+	}
+
+	// Compressed pubkey
+	// https://en.bitcoin.it/wiki/Technical_background_of_version_1_Bitcoin_addresses
+	if pubkeyx, err := hex.DecodeString("0250863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352"); err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	} else if addr, err := PubKeyToAddress(BitcoinPubKeyHash, pubkeyx); err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	} else if addr.String() != "1PMycacnJaSqwwJqjawXBErnLsZ7RkXUAs" {
+		t.Logf("%v != 1PMycacnJaSqwwJqjawXBErnLsZ7RkXUAs", addr.String())
+		t.Logf(hex.EncodeToString(addr.PubKeyHash()))
+		t.Fail()
+		return
+	}
+
+	// Compressed pubkey
+	// https://lists.linuxfoundation.org/pipermail/bitcoin-dev/2012-January/001039.html
+	if pubkeyx, err := hex.DecodeString("034f355bdcb7cc0af728ef3cceb9615d90684bb5b2ca5f859ab0f0b704075871aa"); err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	} else if addr, err := PubKeyToAddress(BitcoinPubKeyHash, pubkeyx); err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	} else if addr.String() != "1Q1pE5vPGEEMqRcVRMbtBK842Y6Pzo6nK9" {
+		t.Logf("%v != 1Q1pE5vPGEEMqRcVRMbtBK842Y6Pzo6nK9", addr.String())
+		t.Logf(hex.EncodeToString(addr.PubKeyHash()))
+		t.Fail()
+		return
+	}
+
+	// Parse address
+	if addr, err := AddressParseBase58("1Q1pE5vPGEEMqRcVRMbtBK842Y6Pzo6nK9"); err != nil {
+		t.Log(err)
+		t.Fail()
+	} else if addr2, err := PubKeyHashToAddress(addr.Version(), addr.PubKeyHash()); err != nil {
+		t.Log(err)
+		t.Fail()
+	} else if addr.String() != addr2.String() {
+		t.Log("address mismatch")
+		t.Fail()
+	}
+}
