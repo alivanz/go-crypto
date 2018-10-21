023163b5491b740470a77f8adbd86c05ba8ef216
Alivan Akbar <alivan1627@gmail.com>
2018-10-21 19:09:53 +0700

    sig-script generator

--- bitcoin/script.go
+++ bitcoin/script.go
@@ -2,6 +2,7 @@ package bitcoin
 
 import (
 	"bytes"
+	"math/big"
 
 	utils "github.com/alivanz/go-utils"
 )
@@ -26,4 +27,45 @@ func P2PKH(pubkeyhash []byte) []byte {
 // Anyone-Can-Spend Outputs
 func AnyoneCanSpent() []byte {
 	return []byte{OP_TRUE}
+}
+
+// P2PKH script signature
+func P2PKHScriptSig(r, s *big.Int, pubkey []byte) []byte {
+	buf := bytes.NewBuffer(nil)
+	w := utils.NewBinaryWriter(buf)
+	w.WriteCompact(DERSignatureFull(r, s))
+	w.WriteCompact(pubkey)
+	return buf.Bytes()
+}
+
+// DER Signature with sighash 0x01
+func DERSignatureFull(r, s *big.Int) []byte {
+	buf := bytes.NewBuffer(nil)
+	w := utils.NewBinaryWriter(buf)
+	w.WriteByte(0x30)
+	w.WriteCompact(DERSignatureHalf(r, s))
+	// Signature hash code
+	w.WriteByte(0x01)
+	return buf.Bytes()
+}
+
+// DER Signature, thats all
+func DERSignatureHalf(r, s *big.Int) []byte {
+	buf := bytes.NewBuffer(nil)
+	buf.Write(DERInt(r))
+	buf.Write(DERInt(s))
+	return buf.Bytes()
+}
+
+// DER format for int with prefix 0x02
+func DERInt(x *big.Int) []byte {
+	buf := bytes.NewBuffer(nil)
+	w := utils.NewBinaryWriter(buf)
+	w.WriteByte(0x02)
+	if x.BitLen()%8 == 0 {
+		w.WriteCompact(append([]byte{0}, x.Bytes()...))
+	} else {
+		w.WriteCompact(x.Bytes())
+	}
+	return buf.Bytes()
 }
